af3baf04a4c05b220ebad9c349198976686f923c
barndogghxc <barndogghxc@yahoo.com>
2018-02-17 23:34:39 -0500

    routing begin

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const logger = require('morgan');
 
 const port = process.env.PORT || 3000;
 
+const bookRoutes = require('./routes/book-routes');
+
 // Init App
 const app = express();
 
@@ -15,8 +17,16 @@ app.use(logger('dev'));
 app.get('/', function(req, res){
 	res.json({
 		message: 'hello goat this is a booklist';
-	})
-})
+	});
+});
+
+// Middleware
+app.use('/books', bookRoutes);
+
+// Error handling
+app.use('*', function(req, res){
+	res.status(404).json('Not Found!');
+});
 
 app.listen(port, function(){
 	console.log(`Server started on port ${port}`);
