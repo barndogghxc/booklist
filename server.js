7f5e5e70865940a856ea453ccb957a7a28a0ff80
barndogghxc <barndogghxc@yahoo.com>
2018-02-17 22:59:44 -0500

    adding comments

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
 
-const port = process.env.PORT || 3000;
+const express = require('express');
 
 const logger = require('morgan');
 
+const port = process.env.PORT || 3000;
+
+// Init App
 const app = express();
 
+// Logging
+app.use(logger('dev'));
+
+// Home Route
 app.get('/', function(req, res){
-	res.send('hello goat this is a booklist');
-});
+	res.json({
+		message: 'hello goat this is a booklist';
+	})
+})
 
 app.listen(port, function(){
 	console.log(`Server started on port ${port}`);
